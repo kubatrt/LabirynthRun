f7c5b0c59339a96211379e691f193acd93f3fbc1
lemur013 <lemur013@interia.pl>
2015-01-04 13:47:26 +0100

    Some fun with new menu

---
+++ Assets/Scripts/GameStates/MenuButtons.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtons : MonoBehaviour {
+
+	public void StartGame()
+	{
+		Application.LoadLevel (3);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit ();
+	}
+
+
+}
---
+++ Assets/Scripts/GameStates/Username.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Username : MonoBehaviour {
+
+	public string username;
+	public UnityEngine.UI.InputField inputFiled;
+	public UnityEngine.UI.Text nameDisplay;
+
+
+	public void NameInput ()
+	{
+		username = inputFiled.text;
+		nameDisplay.text = username;
+		inputFiled.enabled = false;
+	}
+
+	public void ResetInputField ()
+	{
+		username = "";
+		nameDisplay.text = username;
+		inputFiled.text = username;
+		inputFiled.enabled = true;
+	}
+}
