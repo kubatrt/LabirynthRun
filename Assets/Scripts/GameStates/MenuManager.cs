561ee5f965db736c6b9e62a64c37f27db722e814
lemur013 <lemur013@interia.pl>
2015-01-05 12:41:52 +0100

    Totally New Menu

---
+++ Assets/Scripts/GameStates/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour
+{
+	public NewMenu CurrentMenu;
+
+	public void Start()
+	{
+		ShowMenu (CurrentMenu);
+	}
+
+	public void ShowMenu(NewMenu menu)
+	{
+		if(CurrentMenu != null)
+			CurrentMenu.IsOpen = false;
+
+		CurrentMenu = menu;
+		CurrentMenu.IsOpen = true;
+	}
+
+	public void StartGame(int scene)
+	{
+		Application.LoadLevel (scene);
+	}
+
+	public void ExitGame()
+	{
+		Application.Quit ();
+	}
+}
